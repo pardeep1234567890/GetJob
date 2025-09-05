c5ca59a2bd193a02e7ffe2c0d7ea9f964b7dec44
pardeep1234567890 <pardeep905001@gmail.com>
2025-09-05 18:06:48 +0530

    complete the recuriter Dashboard ,manageJob,viewApplication,addjob page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,11 @@ import Applyjob from './pages/Applyjob'
 import Applications from './pages/Applications'
 import RecuriterLogin from './components/RecuriterLogin'
 import { AppContext } from './context/AppContext'
+import Dashboard from './pages/Dashboard'
+import AddJob from './pages/AddJob'
+import ManageJob from './pages/ManageJobs'
+import ViewApplications from './pages/ViewApplications'
+import 'quill/dist/quill.snow.css'
 
 function App() {
   const {showRecuriterLogin} = useContext(AppContext);
@@ -15,6 +20,11 @@ function App() {
         <Route path='/' element={<Home/>}/>
         <Route path='/apply-job/:id' element={<Applyjob/>}/>
         <Route path='/applications' element={<Applications/>}/>
+        <Route path='/dashboard' element={<Dashboard/>}>
+        <Route path='add-job' element={<AddJob/>}/>
+        <Route path='manage-jobs' element={<ManageJob/>}/>
+        <Route path='view-applications' element={<ViewApplications/>}/>
+        </Route>
        </Routes>
       </div>       
   )
---
+++ client/src/pages/AddJob.jsx
@@ -0,0 +1,71 @@
+import React, { useEffect, useRef, useState } from 'react'
+import Quill from 'quill';
+import Theme from 'quill/core/theme';
+import { JobCategories, JobLocations } from '../assets/assets';
+
+function AddJob() {
+  const [title, setTitle] = useState('');
+  const [location, setLocation] = useState('Bangalore');
+  const [category, setCategory] = useState('Programming');
+  const [level, setLevel] = useState('Beginner level');
+  const [salary, setSalary] = useState(0);
+
+  const editorRef = useRef(null)
+  const quillRef = useRef(null)
+  //by using the editorRef we initialize the quillRef
+  useEffect(() => {
+    if (!quillRef.current && editorRef.current) {
+      quillRef.current = new Quill(editorRef.current,
+        { theme: 'snow', }
+      )
+    }
+  }, [])
+
+  return (
+    <form className='container p-4 flex flex-col w-full items-start gap-3'>
+      <div className='w-full'>
+        <p className='mb-2'>Job Title</p>
+        <input type="text" placeholder='Type here' onChange={e => setTitle(e.target.value)} value={title} required
+          className='w-full max-w-lg px-3 py-2 border-2 border-gray-300 rounded' />
+      </div>
+      <div className='w-full max-w-lg '>
+        <p className='my-2'>Job Description</p>
+        <div ref={editorRef}>
+        </div>
+      </div>
+      <div className='flex flex-col sm:flex-row gap-2 w-full sm:gap-8'>
+        <div>
+          <p className='mb-2'>Job Category</p>
+          <select className='w-full px-3 py-2 border-2 border-gray-300 rounded' onChange={e => setCategory(e.target.value)}>
+            {JobCategories.map((category, index) => (
+              <option value={category} key={index}>{category}</option>
+            ))}
+          </select>
+        </div>
+        <div>
+          <p className='mb-2'> Job Location</p>
+          <select className='w-full px-3 py-2 border-2 border-gray-300 rounded' onChange={e => setLocation(e.target.value)}>
+            {JobLocations.map((location, index) => (
+              <option value={location} key={index}>{location}</option>
+            ))}
+          </select>
+        </div>
+        <div>
+          <p className='mb-2'>Job Level</p>
+          <select className='w-full px-3 py-2 border-2 border-gray-300 rounded' onChange={e => setLevel(e.target.value)}>
+            <option value="Beginner level">Beginner level</option>
+            <option value="Intermediate level">Intermediate level</option>
+            <option value="Senior level">Senior level</option>
+          </select>
+        </div>
+      </div>
+      <div>
+        <p className='mb-2'>Job Salary</p>
+        <input min={0} className='w-full px-3 py-2 border-2 border-gray-300 rounded sm:w-[120px]' onChange={e => setSalary(e.target.value)} type="Number" placeholder='2500' />
+      </div>
+      <button className='w-28 py-3 mt-4 bg-black text-white'>ADD</button>
+    </form>
+  )
+}
+
+export default AddJob

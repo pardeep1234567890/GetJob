fa897220897bcfab15b6b2f4a2d5395ef84ce50d
pardeep1234567890 <pardeep905001@gmail.com>
2025-09-09 10:53:40 +0530

    Remove server folder from repository

--- server/config/db.js
+++
@@ -1,8 +0,0 @@
-import mongooose from "mongoose"
-
-// Function to connect to the mongoDB database
-const connectDB = async ()=>{
-    mongooose.connection.on("connected",()=>console.log("Database Connected"))
-    await mongooose.connect(`${process.env.MONGODB_URI}/GetJob`)
-}
-export default connectDB;
--- server/config/instrument.js
+++
@@ -1,9 +0,0 @@
-// Import with `import * as Sentry from "@sentry/node"` if you are using ESM
-import * as Sentry from "@sentry/node";
-
-Sentry.init({
-  dsn: "https://9b63cccf8b4e27b72bbca49ff9bee1c9@o4509977161891840.ingest.us.sentry.io/4509977169100800",
-  // Setting this option to true will send default PII data to Sentry.
-  // For example, automatic IP address collection on events
-  sendDefaultPii: true,
-});
